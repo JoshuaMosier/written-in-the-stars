ce7f7177cdcdac8031676716902bf5cca2189623
Josh Mosier <josh.r.mosier@gmail.com>
2026-03-18 12:58:46 -0400

    analytics + speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,11 @@
 <script>
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
+	injectSpeedInsights();
+
 	let { children } = $props();
 </script>
 
